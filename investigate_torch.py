21b2f0ed0f781314f1961402052b8e4e94483b6d
frknayk <furkanayik@outlook.com>
2020-03-01 18:07:56 +0300

    Jupyter notebook is added for visualization stuff

--- investigate_torch.py
+++ investigate_torch.py
@@ -1,4 +1,4 @@
-# Torcs Environment
+# %%
 import numpy as np
 import math
 import matplotlib.pyplot as plt
@@ -27,8 +27,22 @@ for layer in Brain:
 weights_shape = [layer.shape for layer in weights]
 bias_shape = [layer.shape for layer in biases]
 
-print(weights_shape)
-print(bias_shape)
+# print(weights_shape)
+# print("Weight_names:\n", weight_names)
+# counter = 0
+# for layer in weights:
+#   counter += 1
+# print(counter)
+
+first_layer_weight = weights[0]
+
+def tensor_to_numpy( torch_tensor ):
+  # TODO : HOW TO CONVERT GPU TO NUMPY DIRECTLY
+  return torch_tensor.cpu().numpy()
+
+np_arr = tensor_to_numpy(first_layer_weight)
+
+print(np_arr.shape)
 
 '''
 print("Weight_names:\n", weight_names)
@@ -42,3 +56,4 @@ print("Biases:\n", biases)
 ############## TODO ##########
  1. Visaulize layers (not biases just weights) by MATPLOTLIB
 """
+
